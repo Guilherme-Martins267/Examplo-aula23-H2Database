15b90fdada28ccbb06af09d2b98febf58bb97ada
Guilherme-Martins267 <guihbm.m@gmail.com>
2010-05-06 00:09:30 -0300

    Primeiro exemplo de banco de dados, h2Database

---
+++ src/main/java/MartinsGuilherme/ExamploH2Database/ExamploH2DatabaseApplication.java
@@ -0,0 +1,13 @@
+package MartinsGuilherme.ExamploH2Database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamploH2DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamploH2DatabaseApplication.class, args);
+	}
+
+}
